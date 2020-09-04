dc7ad0bf94bfd09f94df2adb9c0da52775abc8e7
Avinash2110 <avinashshukla0784@gmail.com>
2020-09-04 16:37:19 +0530

    Some LinkedList implementation commit

---
+++ src/Problems/SampleClass.java
@@ -0,0 +1,18 @@
+package Problems;
+
+import ds.LinkedList;
+
+public class SampleClass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		LinkedList list = new LinkedList();
+		list.insertBeginning(4);
+		list.print();
+		list.deleteEnd();
+		list.print();
+
+	}
+
+}
--- src/ds/LinkedList.java
+++ src/ds/LinkedList.java
@@ -1,9 +1,89 @@
 package ds;
 
+//author- Avinash Shukla
+
 public class LinkedList {
 	
+	/**
+	 * Defining the LinkedList Data structure
+	 * It has data variable which contains the integer data
+	 * next variable of type LinkedList which will point to node of type itself
+	 */
+	int data;
+	LinkedList next;
+	LinkedList head;
 	public LinkedList(){
-		int data;
+		head = null;
 	}
-
+	
+	
+	public void insertBeginning(int data){
+		LinkedList node = new LinkedList();
+		node.data = data;
+		
+		if(head==null){
+			head = node;
+		}
+		else{
+			node.next = head;
+			head = node;
+		}
+		
+	}
+	
+	public void insertEnd(int data){
+		LinkedList node = new LinkedList();
+		node.data = data;
+		
+		if(head==null){
+			head = node;
+		}
+		else{
+			LinkedList temp = head;
+			while(temp.next!=null){
+				temp = temp.next;
+			}
+			temp.next = node;
+		}
+		
+	}
+	
+	public void deleteBeginning(){
+		if(head==null){
+			System.out.println("LinkedList is already empty");
+		}
+		else{
+			head = head.next;
+		}
+	}
+	
+	public void deleteEnd(){
+		if(head==null){
+			System.out.println("LinkedList is already empty");
+		}
+		else if(head.next==null){
+			head = null;
+		}
+		else{
+			LinkedList temp = head;
+			while(temp.next.next!=null){
+				temp = temp.next;
+			}
+			temp.next = null;
+		}
+	}
+	
+	public void print(){
+		if(head==null){
+			System.out.println("LinkedList is empty");
+		}
+		else{
+			LinkedList temp = new LinkedList();
+			temp = head;
+			while(temp!=null){
+				System.out.print(temp.data+" ");
+				temp = temp.next;
+			}
+		}
+	}
 }
